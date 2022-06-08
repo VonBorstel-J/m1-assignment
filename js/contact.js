37c893422eba68ba88cee38540f601faf4438938
VonBorstel-J <jordanvonborstel@gmail.com>
2022-06-08 13:58:02 -0700

    Completed most fixes professor has requested, working on JS still

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,7 @@
-const contactSelect = document.getElementById('contactSelect');
+const label1 = document.getElementById('textbox1');
+const label2 = document.getElementById('textbox2'); 
+const email = document.getElementById('email');
+const phone = document.getElementById('phone')
 
+$(document).ready(function() {
+   
