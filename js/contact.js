704832facec1890e8e6a3c943209a6637db7ac89
VonBorstel-J <jordanvonborstel@gmail.com>
2022-06-08 20:31:56 -0700

    Hw finish

--- js/contact.js
+++ js/contact.js
@@ -4,4 +4,20 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone')
 
 $(document).ready(function() {
-   
+    $('email','phone').css('cursor', 'pointer');
+
+    jQuery('email').click(function(){
+        $(this).data('clicked', true);
+      });
+
+if(jQuery('email').data('clicked')) {
+   ('label1').css(display)
+} else {
+    //run function2
+}
+
+
+
+
+
+});
